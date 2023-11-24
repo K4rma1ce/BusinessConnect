216bc652229affd7b375575e3d124fe4f589ff16
Karma <mendeldenis452gmail.com>
2023-11-24 14:08:07 +0300

    added SplashReg which open after registration

--- app/src/main/java/com/example/businessconnect/SignUpAct.kt
+++ app/src/main/java/com/example/businessconnect/SignUpAct.kt
@@ -1,7 +1,9 @@
 package com.example.businessconnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.businessconnect.databinding.ActivitySignInBinding
 import com.example.businessconnect.databinding.ActivitySignUpBinding
 
@@ -11,5 +13,10 @@ class SignUpAct : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    fun onClickSplashReg(view: View){
+        val i = Intent(this, SplashReg::class.java)
+        startActivity(i)
     }
 }
---
+++ app/src/main/java/com/example/businessconnect/SplashReg.kt
@@ -0,0 +1,23 @@
+package com.example.businessconnect
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.businessconnect.databinding.ActivityMainMenuBinding
+import com.example.businessconnect.databinding.ActivitySplashRegBinding
+
+class SplashReg : AppCompatActivity() {
+    lateinit var binding: ActivitySplashRegBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashRegBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    fun onClickGreat(view: View){
+        val i = Intent(this, MainMenu::class.java)
+        startActivity(i)
+    }
+
+}
